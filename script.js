4d974dc1d9bcd6a14dba5ffcf629ad95a54cb52f
nogiamoni <monikanogia14@gmail.com>
2024-08-29 17:49:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function to hide the welcome page and show the portfolio
+function showPortfolio() {
+    document.getElementById('welcome').style.display = 'none';
+    document.getElementById('portfolio').style.display = 'block';
+}
+
+
